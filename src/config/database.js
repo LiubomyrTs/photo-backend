062b74739ff884501a0c79858cf63ad62b78e57e
Liubomyr Tsimbolynets <l.tsimbolynets-c@tamkeentech.sa>
2021-10-13 17:45:09 +0300

    11

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://127.0.0.1:27017/meanauth',
+  database: 'mongodb+srv://liubomyr_1337:1q2w3e4r@testcluster1.r5yb2.mongodb.net/accme?retryWrites=true&w=majority',
   secret: 'yoursecret',
 };
